bc3ae6ff05e996ac1454fff34fd304d81246ad54
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2017-03-08 01:24:36 +0200

    ngModel

--- inventory/src/app/demo-from-sku/demo-from-sku.component.ts
+++ inventory/src/app/demo-from-sku/demo-from-sku.component.ts
@@ -10,20 +10,33 @@ import {skuValidator} from '../validators/sku.validator'
 export class DemoFromSkuComponent implements OnInit {
   myForm: FormGroup
   sku: AbstractControl
+  productName: string
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
       'sku': ['', Validators.compose([
         Validators.required, skuValidator
-      ])]
+      ])],
+      'productName': ['', Validators.required]
     })
 
     this.sku = this.myForm.controls['sku']
+
+    this.sku.valueChanges.subscribe(
+      (value: string) => {
+        console.log('sku changed to: ', value)
+      }
+    )
+
+    this.myForm.valueChanges.subscribe(
+      (form: any) => {
+        console.log('form changed to: ', form)
+      }
+    )
   }
 
-  onSubmit(form: string): void {
-
-    console.log('you submitted value: ', form)
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value)
   }
 
   ngOnInit() {
