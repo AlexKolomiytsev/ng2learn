abc2a3c46150b78b41b6d310b5785c5c2b2f68b9
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2017-03-15 23:23:18 +0200

    working around base paths

---
+++ inventory/src/app/router-app/router-app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'router-app',
+  templateUrl: './router-app.component.html',
+  styleUrls: ['./router-app.component.css']
+})
+export class RouterAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+//TODO: continue with 'Creating the Components' (p. 194)
