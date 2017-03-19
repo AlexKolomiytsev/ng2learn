1282220e7e12db57c3f160b1a412ed4a49ac87fa
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2017-03-19 16:47:43 +0200

    exploring angular cli

--- musicSearchApp/src/app/app.module.ts
+++ musicSearchApp/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './components/root/root.component';
-import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
