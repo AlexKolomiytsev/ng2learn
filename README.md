1ae94cf82410064e0121c95cb93773b654c90a3a
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2017-03-19 16:47:27 +0200

    exploring angular cli

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ng2learn
+
+## Generate new app
+`ng new {APP_NAME} --skip-tests --style=scss`
+
+## Generate new component
+##### will generate the following component:
+###### {APP_NAME}/src/app/components/{componentName}
+
+`ng g component components/{componentName} --spec=false`
--- musicSearchApp/src/app/app.module.ts
+++ musicSearchApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './components/root/app.component';
+import { AppComponent } from './components/root/root.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
