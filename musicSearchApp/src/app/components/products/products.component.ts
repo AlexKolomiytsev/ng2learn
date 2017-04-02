0aa5078dac287a6ca00e5878de07efc32730c512
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2017-04-02 21:25:05 +0300

    todo

--- musicSearchApp/src/app/components/products/products.component.ts
+++ musicSearchApp/src/app/components/products/products.component.ts
@@ -71,4 +71,4 @@ export const routes: Routes = [
 })
 export class ProductsComponentModule {}
 
-//TODO: start with 'Data Architecture in Angular 2' (p. 270)
+//TODO: start with 'Chat App Overview' (p. 274)
