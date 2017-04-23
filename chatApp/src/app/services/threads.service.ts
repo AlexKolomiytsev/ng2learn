f45afb29c942ea07be887b06c46e64d19fe70b7e
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2017-04-23 20:37:28 +0300

    todo

--- chatApp/src/app/services/threads.service.ts
+++ chatApp/src/app/services/threads.service.ts
@@ -88,4 +88,4 @@ export const THREADS_PROVIDERS: Array<any> = [
 
  */
 
-//TODO: continue with 'A chronological list of Threads, newest-first (in orderedthreads)' (p. 301)
+//TODO: continue with 'Introduction to Redux with TypeScript' (p. 334)
